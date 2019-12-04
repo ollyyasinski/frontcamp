04e1d8ff44adbaa054cb57f8a8cd045928f56031
Volha Yasinskaya <vyasinskaya@slb.com>
2019-12-04 18:26:07 +0300

    WIP: add facebook login

---
+++ nodeJS/authentication/facebook-strategy.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const LocalStrategy = require("passport-facebook");
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      profileFields: ["email", "name"]
+    },
+    function(accessToken, refreshToken, profile, done) {
+      const { email, first_name, last_name } = profile._json;
+      const userData = {
+        email,
+        firstName: first_name,
+        lastName: last_name
+      };
+      new userModel(userData).save();
+      done(null, profile);
+    }
+  )
+);
