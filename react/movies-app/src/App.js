f0292db11ebb8cac1b309fe4fe925939015d01e7
ollyysinski <you@example.com>
2019-12-16 10:14:46 +0300

    add router

--- react/movies-app/src/App.js
+++ react/movies-app/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import SearchPage from "./containers/search-page/search-page";
 import ErrorBoundary from "./components/error-boundary/error-boundary";
-// import MovieDetailsPage from "./containers/movie-details-page/movie-details-page";
+import MovieDetailsPage from "./containers/movie-details-page/movie-details-page";
 
 function App() {
   return (
     <ErrorBoundary>
       <div className="App">
-        <SearchPage />
-        {/* <MovieDetailsPage /> */}
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={SearchPage} />
+            <Route exact path="/movies" component={MovieDetailsPage} />
+          </Switch>
+        </BrowserRouter>
       </div>
     </ErrorBoundary>
   );
--- react/movies-app/src/index.js
+++ react/movies-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import { Provider } from "react-redux";
 import configureStore from "./store/configure-store";
 
